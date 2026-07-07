a98ccbd3998e07f4cd9ed16b20d1e214798ec518
Simon Lau <simon.lau.sg@gmail.com>
2026-07-07 23:39:34 +0800

    test: add more tests

--- ex02/test.c
+++ ex02/test.c
@@ -29,14 +29,29 @@ void	test_negative(void)
 {
 	int	nb;
 
-	nb = -42;
+	nb = -4242;
 	ft_putnbr(nb);
 }
 
+void	test_negative_single(void)
+{
+	int	nb;
+
+	nb = -7;
+	ft_putnbr(nb);
+}
+
+#include <stdio.h>
+
 int	main(void)
 {
-	// test_42();
-	// test_zero();
-	// test_single_digit();
+	test_42();
+	printf("%s", "\n");
+	test_zero();
+	printf("%s", "\n");
+	test_single_digit();
+	printf("%s", "\n");
 	test_negative();
+	printf("%s", "\n");
+	test_negative_single();
 }
