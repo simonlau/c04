3daa12dc6e7105c6fc4a2b2be00a6632a1603b51
Simon Lau <simon.lau.sg@gmail.com>
2026-07-07 08:59:48 +0800

    feat: did ex01

---
+++ ex01/test.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <string.h>
+
+void	ft_putstr(char *str);
+
+void	test_normal(void)
+{
+	char	*str;
+
+	str = "simon\n";
+	ft_putstr(str);
+}
+
+void	test_empty(void)
+{
+	char	*str;
+
+	str = "";
+	ft_putstr(str);
+}
+
+int	main(void)
+{
+	test_empty();
+	test_normal();
+}
