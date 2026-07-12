1627914c56065cf28ec5d27077e3b7b931466a9b
Simon Lau <simon.lau.sg@gmail.com>
2026-07-12 10:03:28 +0800

    ci: change test to use pipe buffer

--- ex01/test.c
+++ ex01/test.c
@@ -1,30 +1,68 @@
 #include <assert.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
-void	ft_putstr(char *str);
+enum
+{
+	READ_END,
+	WRITE_END
+};
+
+typedef struct s_capture
+{
+	int		pipe_fds[2];
+	int		stdout_backup;
+	char	buf[1024];
+	ssize_t	len;
+}			t_capture;
+
+void		ft_putstr(char *str);
+
+void	setup(t_capture *c)
+{
+	c->stdout_backup = dup(STDOUT_FILENO);
+	pipe(c->pipe_fds);
+	dup2(c->pipe_fds[WRITE_END], STDOUT_FILENO);
+}
+
+void	teardown(t_capture *c)
+{
+	close(c->pipe_fds[WRITE_END]);
+	dup2(c->stdout_backup, STDOUT_FILENO);
+	c->len = read(c->pipe_fds[READ_END], c->buf, sizeof(c->buf) - 1);
+	c->buf[c->len] = '\0';
+	close(c->pipe_fds[READ_END]);
+}
 
 void	test_normal(void)
 {
-	char	*str;
+	t_capture	c;
 
-	str = "simon\n";
-	ft_putstr(str);
+	setup(&c);
+	ft_putstr("simon");
+	teardown(&c);
+	assert(strcmp("simon", c.buf) == 0);
 }
 
 void	test_empty(void)
 {
-	char	*str;
+	t_capture	c;
 
-	str = "";
-	ft_putstr(str);
+	setup(&c);
+	ft_putstr("");
+	teardown(&c);
+	assert(strcmp("", c.buf) == 0);
 }
 
 void	test_one_char(void)
 {
-	char	*str;
+	t_capture	c;
 
-	str = "?";
-	ft_putstr(str);
+	setup(&c);
+	ft_putstr("?");
+	teardown(&c);
+	assert(strcmp("?", c.buf) == 0);
 }
 
 int	main(void)
