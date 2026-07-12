b829c9f9fc1b99c7d3d8fdddb32f9515ffa0bbc1
Simon Lau <simon.lau.sg@gmail.com>
2026-07-12 09:33:37 +0800

    test: add empty test

--- ex00/test.c
+++ ex00/test.c
@@ -5,23 +5,16 @@ int		ft_strlen(char *str);
 
 void	test_normal(void)
 {
-	char	*str;
-	int		result;
-
-	str = "simon";
-	result = ft_strlen(str);
-	assert(result == 5);
+	assert(5 == ft_strlen("simon"));
 }
 
 void	test_empty(void)
 {
-	char	*str;
-
-	str = "";
-	assert(0 == ft_strlen(str));
+	assert(0 == ft_strlen(""));
 }
 
 int	main(void)
 {
 	test_normal();
+	test_empty();
 }
